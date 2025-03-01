d9248bddc95cf124ea557ebfc9fc0c2aab604be1
Your Name <you@example.com>
2025-03-01 11:39:07 -0600

    User authentication functionality added. Login botton functional now.

--- src/Login.java
+++ src/Login.java
@@ -4,6 +4,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.sql.*;
 
 public class Login extends Application{
 
@@ -22,9 +23,12 @@ public class Login extends Application{
 		loginButton.setOnAction(e -> {
 			String username = userNameField.getText();
 			String password = passwordField.getText();
-			String message = "Welcome to the JavaFX login app!\nLogin button is not yet functional.";
 			
-			messageLabel.setText(message);
+			if(authenticateUser(username, password)) {
+				messageLabel.setText("Login Successful!");
+			}else {
+				messageLabel.setText("Login Failed! Check your username and/or password.");
+			}
 		});
 		
 		//layout
@@ -38,6 +42,29 @@ public class Login extends Application{
 		primaryStage.setTitle("Login");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	/*
+	 * This method checks if username and password exist in the database.
+	 * If exist, it returns True, if not, returns false.
+	 */
+	private boolean authenticateUser(String username, String password) {
+		String databaseURL = "jdbc:mysql://localhost:3306/javafx_login";
+		String databaseUser = "root"; //replace with your mysql database user
+		String databasePassword = "Saanvi2015"; //replace with your mysql database password
+		String sqlQuery = "SELECT * FROM users WHERE username = ? and password = ?";
+		try {
+			Connection conn = DriverManager.getConnection(databaseURL, databaseUser, databasePassword);
+			PreparedStatement statement = conn.prepareStatement(sqlQuery);
+			statement.setString(1, username);
+			statement.setString(2, password);
+			
+			ResultSet rs = statement.executeQuery();
+			return rs.next();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public static void main(String[] args) {
