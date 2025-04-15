65223566decda509d2c2cdb0a3ff30a99f2e8262
Your Name <you@example.com>
2025-04-15 11:43:15 -0500

    Created a main class, the entry point to the program.

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import controller.MainController;
+import javafx.stage.Stage;
+/**
+ * 
+ * @author Dilli Khatiwoda
+ * main controller class. This is the entry point to the program
+ *
+ */
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		//To Do: create MainController class in controller package.
+		MainController controller = new MainController(primaryStage);
+		Scene scene = new Scene(controller.getView(), 800, 600);
+		
+		primaryStage.setTitle("Idea Submission App");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+public static void main (String[] args) {
+	launch(args);
+}
+	
+}
+
