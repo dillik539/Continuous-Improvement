8ede8fd3147caa7fe985cb2002e8ef1621409f37
Your Name <you@example.com>
2025-04-16 11:38:24 -0500

    Added userID property for the Idea object. Also, added getter function.

--- src/model/Idea.java
+++ src/model/Idea.java
@@ -1,18 +1,25 @@
 package model;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Idea {
+	private final SimpleIntegerProperty userID;
 	private final SimpleStringProperty shortDescription;
 	private final SimpleStringProperty fullIdea;
 	private final SimpleStringProperty dateSubmitted;
 	private final SimpleStringProperty status;
 
-	public Idea(String shortDescription, String fullIdea, String dateSubmitted, String status) {
+	public Idea(Integer userID,String shortDescription, String fullIdea, String dateSubmitted, String status) {
+		this.userID = new SimpleIntegerProperty(userID);
 		this.shortDescription = new SimpleStringProperty(shortDescription);
 		this.fullIdea = new SimpleStringProperty(fullIdea);
 		this.dateSubmitted = new SimpleStringProperty(dateSubmitted);
 		this.status = new SimpleStringProperty(status);
+	}
+	
+	public Integer getUserID() {
+		return userID.get();
 	}
 
 	public String getShortDescription() {
