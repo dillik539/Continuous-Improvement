6ce6471995fef427c26e2146e505d2a1175e333f
Your Name <you@example.com>
2025-12-02 17:14:20 -0600

    A feature to obtain peer recognitions is added in the Database model class.

--- src/model/Database.java
+++ src/model/Database.java
@@ -3,7 +3,11 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Database {
 	private static final String URL = "jdbc:mysql://localhost:3306/IdeaSubmissionDB";
@@ -33,5 +37,28 @@ public class Database {
 			
 			stmt.executeUpdate();
 		}
+	}
+	
+	//get all recognition records (for admin use)
+	public static ObservableList<PeerRecognition> getAllPeerRecognitions() throws Exception {
+		ObservableList<PeerRecognition> list = FXCollections.observableArrayList();
+		String getRecognitonStmt = "SELECT peer_recognition ORDER BY recognition_date DESC";
+		try(Connection conn = getConnection(); PreparedStatement stmt = conn.prepareStatement(getRecognitonStmt)){
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				list.add(new PeerRecognition(
+						rs.getInt("id"),
+						rs.getInt("recognized_user_id"),
+						rs.getInt("recognizer_user_id"),
+						rs.getString("recognized_username"),
+						rs.getString("recognizer_name"),
+						rs.getString("product_line"),
+						rs.getString("recognition_text"),
+						rs.getString("mindset"),
+						rs.getDate("recognition_date").toLocalDate()
+						));
+			}
+		}
+		return list;
 	}
 }
