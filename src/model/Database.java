6ef2f5b355da7c70074312f38b23e18016e53d7d
Your Name <you@example.com>
2025-12-01 20:16:19 -0600

    A method to insert peer recognition is added in the Database.java.

--- src/model/Database.java
+++ src/model/Database.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
@@ -11,5 +12,26 @@ public class Database {
 
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+	
+	//inserts recognition record
+	public static void insertPeerRecognition(PeerRecognition pr) throws Exception {
+		String insertStmt = "INSERT INTO peer_recognition +"
+				+ "(recognized_user_id, recognizer_user_id, recognized_username,+"
+				+ "recognizer_name, recognition_text, mindset, recognition_date) +"
+				+ "VALUES (?,?,?,?,?,?,?,?)";
+		
+		try (Connection conn = getConnection();
+			PreparedStatement stmt = conn.prepareStatement(insertStmt)){
+			stmt.setInt(1, pr.getRecognizedUserId());
+			stmt.setInt(2, pr.getRecognizerUserId());
+			stmt.setString(3, pr.getRecognizedUsername());
+			stmt.setString(4, pr.getRecognizerName());
+			stmt.setString(5, pr.getProductLine());
+			stmt.setString(6, pr.getMindset());
+			stmt.setDate(7, java.sql.Date.valueOf(pr.getRecognitionDate()));
+			
+			stmt.executeUpdate();
+		}
 	}
 }
